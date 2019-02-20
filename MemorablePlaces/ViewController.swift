1a78b5cae3643d97079ca72228fffd92528c374e
Gladston Joseph <gladstonjoseph@gladstons-macbook-air.local>
2019-02-20 13:30:55 +0530

    Initial Commit

---
+++ MemorablePlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemorablePlaces
+//
+//  Created by Gladston Joseph on 2/20/19.
+//  Copyright © 2019 Gladston Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
